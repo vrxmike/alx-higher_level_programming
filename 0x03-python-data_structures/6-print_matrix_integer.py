a2ea9d8e43d4557a852850156c3c3ac6b1a582ab
vrxmike <alvinvrxmike@icloud.com>
2022-09-26 18:56:33 -0700

    6-Task-print_matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""a function that prints a matrix of integers."""
+
+
+
+def print_matrix_integer(matrix=[[]]):
+    length = len(matrix)
+    for x in range(length):
+        for number in matrix[x]:
+            if number != matrix[x][len(matrix[x])-1]:
+                print("{:d}".format(number), end=" ")
+            else:
+                print("{:d}".format(number), end="")
+        print("")
