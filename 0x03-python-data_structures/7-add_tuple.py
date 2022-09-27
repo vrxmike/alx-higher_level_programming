a6873f01a42295037120023c058f394bd8d0c818
vrxmike <alvinvrxmike@icloud.com>
2022-09-26 19:58:22 -0700

    7-Task-hotifix

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,14 +2,35 @@
 
 """a function that adds 2 tuples."""
 
-if len(tuple_a) < 2:
-    if len(tuple_a) == 0:
-        tuple_a = 0, 0
+def add_tuple(tuple_a=(), tuple_b=()):
+    a1 = 0
+    a2 = 0
+    b1 = 0
+    b2 = 0
+    error = ''
+    if len(tuple_a) > 1:
+        a1 = tuple_a[0]
+        a2 = tuple_a[1]
     else:
-        tuple_a = tuple_a[0], 0
-if len(tuple_b) < 2:
-    if len(tuple_b) == 0:
-        tuple_b = 0, 0
+        try:
+            a1 = tuple_a[0]
+        except Exception:
+            error = Exception
+        try:
+            a2 = tuple_a[1]
+        except Exception:
+            error = Exception
+    if len(tuple_b) > 1:
+        b1 = tuple_b[0]
+        b2 = tuple_b[1]
     else:
-        tuple_b = tuple_b[0], 0
-return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+        try:
+            b1 = tuple_b[0]
+        except Exception:
+            error = Exception
+        try:
+            b2 = tuple_b[1]
+        except Exception:
+            error = Exception
+    del error
+    return (a1 +b1, a2 + b2)
