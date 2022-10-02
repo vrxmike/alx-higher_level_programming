873625414257391b18ab96d8c61a2a391f29a244
vrxmike <alvinvrxmike@icloud.com>
2022-10-02 16:11:35 -0700

    13-Task-weighted_average

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""a function that returns the weighted
+avarage of all integers tuple (<score>, <weight>)"""
+
+def weight_average(my_list=[]):
+    if len(my_list) == 0:
+        return 0
+    wa_sum = 0
+    div = 0
+    for score, weight in my_list:
+        wa_sum += (score * weight)
+        div += weight
+    return wa_sum / div
