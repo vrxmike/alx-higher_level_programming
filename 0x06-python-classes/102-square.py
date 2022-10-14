43e94922fa2a62002d6a0108faf62e5f1a18f23e
vrxmike <alvinvrxmike@icloud.com>
2022-10-14 12:50:34 -0700

    9-Task-fix

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -50,7 +50,7 @@ class Square:
 
     def __ne__(self, other):
         """Define the < comparison to a Square."""
-        return self.area90 != other.area()
+        return self.area() != other.area()
 
     def __lt__(self, other):
         """Define the < comparison to a Square."""
