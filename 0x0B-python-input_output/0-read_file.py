6dd19ba3a28bee33e295d69900ea61799d19a63a
vrxmike <alvinvrxmike@icloud.com>
2022-10-25 18:53:16 +0000

    0-Task

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Opening a file and reading it's contents
+
+"""
+
+
+def read_file(filename=""):
+	"""Reading the file using with"""
+	with open(filename, 'r', encoding="utf-8") as f:
+		print(f.read(), nd='')
