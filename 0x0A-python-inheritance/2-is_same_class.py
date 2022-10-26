a223e9f3f5f5add55066a574c22b8af9e220bb4d
vrxmike <alvinvrxmike@icloud.com>
2022-10-26 16:26:26 +0000

    2-Task

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Checks for some class or inherit from"""
+
+
+def is_kind_of_class(obj, a_class):
+	"""returns True if object is an instance of,
+	or if the object is an instance of a class that
+	inherited from, the specified class; otherwise False"""
+
+	return isinstance(obj, a_class) or isinstance(type(obj), a_class)
