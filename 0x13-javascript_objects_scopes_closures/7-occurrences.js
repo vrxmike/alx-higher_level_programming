cfe39fa701f4dd6acfa071cbc163a99213cd7a75
vrxmike <alvinvrxmike@icloud.com>
2023-03-17 08:33:23 -0700

    7-Task-update

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -2,9 +2,9 @@
 exports.nbOccurences = function (list, searchElement) {
   let nOccurrences = 0;
   for (let i = 0; i < list.length; i++) {
-   if (searchElement === list[i]) {
-     nOccurrences++;
-   }
+    if (searchElement === list[i]) {
+      nOccurrences++;
+    }
   }
   return nOccurrences;
 };
