9e96f25190b5be57582d76f2db626e4e2f1e6298
vrxmike <alvinvrxmike@icloud.com>
2023-03-17 08:12:07 -0700

    7-Task-Hotfix

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-exports.nbOccurances = function (list, searchElement) {
-  let nOccurances = 0;
+exports.nbOccurences = function (list, searchElement) {
+  let nOccurrences = 0;
   for (let i = 0; i < list.length; i++) {
    if (searchElement === list[i]) {
      nOccurrences++;
