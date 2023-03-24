9c4ca4f05dfd41b17196e9d3bd03b2beaf670e32
vrxmike <alvinvrxmike@icloud.com>
2023-03-24 10:07:57 -0700

    4-Task-upd

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
     cur.execute("""SELECT cities.id, cities.name, states.name FROM
-                cities INNER JOIN ON states.id=citi4-cities_by_state.py.state_id""")
+                cities INNER JOIN states ON states.id=cities.state_id""")
     rows = cur.fetchall()
     for row in rows:
         print(row)
