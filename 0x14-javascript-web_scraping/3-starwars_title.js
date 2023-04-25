6d4b9ec8ef39e7e67290cf1fe3144a4588fd8b1b
vrxmike <alvinvrxmike@icloud.com>
2023-04-25 22:45:52 +0000

    3-Task-Fix

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,7 +2,7 @@
 
 const request = require('request');
 const episodeNum = process.argv[2];
-const API_URL = 'https"//swapi-api.hbtn.io/api/films/';
+const API_URL = 'https://swapi-api.hbtn.io/api/films/';
 
 request(API_URL + episodeNum, function (err, response, body) {
   if (err) {
