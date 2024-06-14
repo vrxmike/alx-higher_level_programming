c8740ddac7849f0087c7fcb46b7b982782cad7b5
vrxmike <alvinvrxmike@icloud.com>
2024-06-14 13:44:08 -0700

    hotfix 102-magic_calculation.py

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # Author - Alvin O Mike
 
-def magic_calculate(a, b, c):
+def magic_calculation(a, b, c):
     """Match bytecode provided by Holberton School."""
     if a < b:
         return (c)
