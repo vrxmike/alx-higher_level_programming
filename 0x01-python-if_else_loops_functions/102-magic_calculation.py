4113f0f2b47249fe753a88ec51d6fef1123bd03a
vrxmike <alvinvrxmike@icloud.com>
2024-06-14 13:38:35 -0700

    add 102-magic_calculation.py

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# Author - Alvin O Mike
+
+def magic_calculate(a, b, c):
+    """Matchbytecode provided by Holberton School."""
+    if a < b:
+        return (c)
+    if c > b:
+        return (a + b)
+    return (a*b - c)
